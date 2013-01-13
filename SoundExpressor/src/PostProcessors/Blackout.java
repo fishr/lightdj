15b23cbf36a4a8e0bd6ef966334f41dde6b263df
Steve Levine <sjlevine@mit.edu>
2013-01-12 19:06:07 -0500

    Added rise/fall time control for the black-out post processor!

--- SoundExpressor/src/PostProcessors/Blackout.java
+++ SoundExpressor/src/PostProcessors/Blackout.java
@@ -14,14 +14,20 @@ import Visualizors.RGBGradientLinear;
  */
 public class Blackout extends PostProcessor {
 
-	protected GenericKnob lengthKnob;
+	protected GenericKnob fallKnob;
+	protected GenericKnob riseKnob;
 	protected boolean active;
-	protected long startTime;
+	protected float brightness;
+	
+	protected static float BRIGHTNESS_HIGH_CUTOFF = 0.98f;
+	protected static float BRIGHTNESS_LOW_CUTOFF = 0.02f;
+	
+	protected static float K_GAIN = 30.0f;
 	
 	
 	public Blackout(double updatesPerSecond) {
 		super(updatesPerSecond);
-		
+		brightness = 1.0f;
 	}
 
 	@Override
@@ -35,28 +41,66 @@ public class Blackout extends PostProcessor {
 		active = false;
 		
 		// Make a user control
-		//lengthKnob = new GenericKnob(0.25f, 40, "Length");
-		//requestUserControl(lengthKnob);
+		fallKnob = new GenericKnob(0.4f, 40, "Fall");
+		riseKnob = new GenericKnob(0.4f, 40, "Rise");
+		requestUserControl(fallKnob);
+		requestUserControl(riseKnob);
 		
 	}
 
 	@Override
 	public void postProcess(ColorOutput colorOutput, FeatureList featureList) {
 		
+		float delta;
+		float gain;
+		float input;
 		
 		// Currently not active. Should it became active?
 		if (((Double) featureList.getFeature("KEY_BACKSPACE")) == 1.0) {
 			// ACTIVATE!
+			input = -1.0f;
+			gain = fallKnob.getValue();
+		} else {
+			input = 1.0f;
+			gain = riseKnob.getValue();
+		}
+		
+		// Compute the next brightness!
+		delta = (float) (K_GAIN * gain / UPDATES_PER_SECOND) * input;
+		brightness += delta;
+		// Make sure we don't go out of [0.0, 1.0]
+		if (brightness > 1.0f) {
+			brightness = 1.0f;
+		} else if (brightness < 0.0f) {
+			brightness = 0.0f;
+		}
+		
+		
+		if (brightness <= BRIGHTNESS_HIGH_CUTOFF) {
 			active = true;
-			startTime = System.currentTimeMillis();
 		} else {
 			active = false;
 		}
 		
-		
 		// Continue a burst that is currently running
 		if (active) {
-			colorOutput.allOff();
+			if (brightness >= BRIGHTNESS_LOW_CUTOFF) {
+				for(int light = 0; light < ColorOutput.NUM_FRONT_RGB_PANELS*ColorOutput.NUM_LEDS_PER_RGB_BOARD; light++) {
+					colorOutput.rgbLightsFront[light] = RGBGradientLinear.linearGradient(Color.BLACK, colorOutput.rgbLightsFront[light], brightness);
+				}
+				for(int light = 0; light < ColorOutput.NUM_REAR_RGB_PANELS*ColorOutput.NUM_LEDS_PER_RGB_BOARD; light++) {
+					colorOutput.rgbLightsRear[light] = RGBGradientLinear.linearGradient(Color.BLACK, colorOutput.rgbLightsRear[light], brightness);
+				}
+				for(int light = 0; light < ColorOutput.NUM_UV_LIGHTS; light++) {
+					colorOutput.uvLights[light] *= brightness;
+					colorOutput.whiteLights[light] *= brightness;
+				}
+				
+				
+			} else {
+				// If we're basically dark, just turn everything off!
+				colorOutput.allOff();
+			}
 		}
 		
 	}
